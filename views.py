b4c04ccb4ec82630386276c4767721fc0780e4d3
vagrant <vagrant@vagrant.vm>
2018-12-03 21:27:53 +0000

    testing add/get category functions and deleted unnecessary db files

--- views.py
+++ views.py
@@ -2,7 +2,7 @@ import time
 from functools import update_wrapper
 from flask import request, g
 from flask import Flask, jsonify 
-from models import Base, Item
+from models import Base, Category 
 
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -11,28 +11,38 @@ from sqlalchemy import create_engine
 
 import json
 
-engine = create_engine('sqlite:///bargainMart.db')
+engine = create_engine('sqlite:///cookBook.db')
 
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 app = Flask(__name__)
 
-def addCategory(name)
+def addCategory(name):
     session = DBSession()
     #check if category already exists
-    category = session.query(Category).filter_by(nane=name)
+    category = session.query(Category).filter_by(name=name).all()
     if category == []:
         category = Category(name=name)
         session.add(category)
         session.commit()
         print("Item added, here are all the categories so far")
-        categories = session.query(category).all()
-        print categories
+        categories = session.query(Category).all()
+        for cat in categories:
+            print(cat)
     else:
         print("That category is already in the db!")
+        for cat in category:
+            print(cat)
+
+def getCategories():
+    session = DBSession()
+    categories = session.query(Category).all()
+    print(categories)
+
+addCategory('Georgian')
 
 app = Flask(__name__)
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host = '0.0.0.0', port = 8234)
