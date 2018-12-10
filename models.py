b98ca9b68b343e26510cf3a98a2791b2f15592b3
vagrant <vagrant@vagrant.vm>
2018-12-10 20:44:39 +0000

    added serialize function to db classes for json api endpoints

--- models.py
+++ models.py
@@ -14,17 +14,36 @@ class User(Base):
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
-    
+    @property
+    def serialize(self):
+        return {
+            'username': self.name,
+            'email': self.email,
+            'picture': self.picture
+        }
+
 class Author(Base):
     __tablename__ = 'author'
     id = Column(Integer, primary_key=True)
     first_name = Column(String(100))
     last_name = Column(String(100))
+    @property
+    def serialize(self):
+        return {
+            'author_first_name': self.first_name,
+            'author_last_name': self.last_name
+        }
 
 class Category(Base):
     __tablename__ = 'category'
     id = Column(Integer, primary_key=True)
     name = Column(String(500))
+    
+    @property
+    def serialize(self):
+        return {
+            'name': return self.name
+        }
 
 class Book(Base):
     __tablename__ = 'book'
@@ -36,7 +55,17 @@ class Book(Base):
     image_url = Column(String(500))
     user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
 
+    @property
+    def serialize(self):
+        return {
+            'book_title': self.title,
+            'author_id': self.author_id,
+            'description': self.description,
+            'category_id': self.category,
+            'image_url': self.image_url,
+            'user_id': self.user_id
+        }
+        
 engine = create_engine('sqlite:///cookBook.db')
-
 
 Base.metadata.create_all(engine)
