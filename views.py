c34e29b577db9965c4125e0dd7b01698051cc0ea
vagrant <vagrant@vagrant.vm>
2018-12-11 15:20:06 +0000

    fixed syntax error

--- views.py
+++ views.py
@@ -95,7 +95,7 @@ def fbconnect():
 @app.route('/fbdisconnect')
 def fbdisconnect():
     facebook_id = login_session['facebook_id']
-    access_token = login_session['acess_token']
+    access_token = login_session['access_token']
     url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id,access_token)
     h = httplib2.Http()
     result = h.request(url, 'DELETE')[1]
